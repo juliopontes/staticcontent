c5ca6e42b1c39c59a7b9b1297bb820a9b7284aa6
Julio Pontes <juliopfneto@gmail.com>
2011-11-18 00:34:56 -0200

    fixes some reference links

--- admin/controllers/html.php
+++ admin/controllers/html.php
@@ -67,7 +67,8 @@ class StaticContentControllerHTML extends JController
 	
 	public function generate()
 	{
-		$items = JApplication::getInstance('site')->getMenu()->getMenu();
+		$site = JApplication::getInstance('site');
+		$items = $site->getMenu()->getMenu();
 		$countFiles = 0;
 		
 		$params = JComponentHelper::getParams('com_staticcontent');
@@ -88,7 +89,8 @@ class StaticContentControllerHTML extends JController
 			$url = JURI::root();
 			$url .= 'index.php?'.$uri->getQuery();
 			
-			if ($uri->getVar('option') == 'com_user' && $uri->getVar('view') == 'profile') continue;
+			$authorize = $site->getMenu()->authorise($item->id);
+			if ( ($uri->getVar('option') == 'com_user' && $uri->getVar('view') == 'profile' ) || !$authorize) continue;
 			
 			$file_source = (!$item->home) ? $item->alias.'.html' : 'index.html' ;
 			$path_source = $this->base_directory.DS;
@@ -101,6 +103,12 @@ class StaticContentControllerHTML extends JController
 			$scripts = $domDocument->getElementsByTagName('script');
 			$images = $domDocument->getElementsByTagName('img');
 			
+			$intersect = array_intersect(explode(DS,JPATH_ROOT), explode('/',$uri->getPath()));
+			$basePath = '/'.implode('/',$intersect).'/';
+			
+			$body = str_replace($uri->root(),'',$body);
+			$body = str_replace($basePath,'',$body);
+			
 			foreach ($images as $img) {
 				$this->copyFile($img, 'src');
 			}
@@ -111,12 +119,104 @@ class StaticContentControllerHTML extends JController
 				$this->copyFile($link, 'href');
 			}
 			
+			//$body = $this->fixLinks($body,$items);
+			
 			$pathFileSource = $path_source.$file_source;
-			if (JFile::write($pathFileSource, $body)) $countFiles++;;
+			if (JFile::write($pathFileSource, $body)) $countFiles++;
 		}
 		
 		JFactory::getApplication()->enqueueMessage(JText::sprintf('COM_STATICCONTENT_HTML_GENERATE_HTML',$countFiles));
 		$this->display();
+	}
+	
+
+	public static function _($url, $xhtml = true, $ssl = null)
+	{
+		// Get the router.
+		$app	= JApplication::getInstance('site');
+		$router	= $app->getRouter();
+
+		// Make sure that we have our router
+		if (!$router) {
+			return null;
+		}
+
+		if ((strpos($url, '&') !== 0) && (strpos($url, 'index.php') !== 0)) {
+			return $url;
+		}
+
+		// Build route.
+		$uri = $router->build($url);
+		$url = $uri->toString(array('path', 'query', 'fragment'));
+
+		// Replace spaces.
+		$url = preg_replace('/\s/u', '%20', $url);
+
+		/*
+		 * Get the secure/unsecure URLs.
+		 *
+		 * If the first 5 characters of the BASE are 'https', then we are on an ssl connection over
+		 * https and need to set our secure URL to the current request URL, if not, and the scheme is
+		 * 'http', then we need to do a quick string manipulation to switch schemes.
+		 */
+		if ((int) $ssl) {
+			$uri = JURI::getInstance();
+
+			// Get additional parts.
+			static $prefix;
+			if (!$prefix) {
+				$prefix = $uri->toString(array('host', 'port'));
+			}
+
+			// Determine which scheme we want.
+			$scheme	= ((int)$ssl === 1) ? 'https' : 'http';
+
+			// Make sure our URL path begins with a slash.
+			if (!preg_match('#^/#', $url)) {
+				$url = '/'.$url;
+			}
+
+			// Build the URL.
+			$url = $scheme.'://'.$prefix.$url;
+		}
+
+		if ($xhtml) {
+			$url = htmlspecialchars($url);
+		}
+
+		return $url;
+	}
+	
+	private function fixLinks($body,$items)
+	{
+		foreach ($items as $item) {
+			
+			//not copy external links
+			if (!JURI::isInternal($item->link) || $item->access != 1) continue;
+			
+			ComStaticContentHelperMenu::getLink($item);
+			if (!isset($item->flink)) continue;
+			$item->flink = str_replace('administrator/','',$item->flink);
+			
+			if (!isset($item->flink)) continue;
+			
+			$url = self::_($item->flink);
+			
+			$base = JUri::root();
+			
+			$url = str_replace($base.'/','',$url);
+			
+			$body = $this->fixLink($url, $item->alias, $body);
+		}
+		
+		return $body;
+	}
+	
+	private function fixLink($url,$alias,$body)
+	{
+		$body = str_replace($url,$alias.'.html',$body);
+		
+		return $body;
 	}
 	
 	private function copyFile($node,$attribute)
@@ -134,9 +234,11 @@ class StaticContentControllerHTML extends JController
 			
 			if(JURI::isInternal($url) == $interno) return;
 			
-			$path = str_replace(end( explode(DS,JPATH_ROOT) ).'/','',$uri->getPath());
+			$intersect = array_intersect(explode(DS,JPATH_ROOT), explode('/',$uri->getPath()));
+			$path = str_replace(implode('/',$intersect).'/','',$uri->getPath());
 			
-			$sourceFilePath = JPATH_ROOT.$path;
+			
+			$sourceFilePath = JPath::clean(JPATH_ROOT.$path);
 			$filePath = JPath::clean($path_source.$path);
 	
 			if (JFile::exists($sourceFilePath)) {
