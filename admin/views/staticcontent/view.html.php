5c17a4fb1ec861f733aeb72dda0ee177f910c079
Matt Thomas <matt@betweenbrain.com>
2012-03-09 20:58:23 -0500

    Correcting parse error.

--- admin/views/staticcontent/view.html.php
+++ admin/views/staticcontent/view.html.php
@@ -10,20 +10,20 @@ class StaticContentViewStaticContent extends JView
 	{
 		$params = JComponentHelper::getParams('com_staticcontent');
 		$base_directory = $params->get('base_directory');
-		
+
 		$this->sef = JFactory::getConfig()->get('sef',0);
-		
+
 		$this->base_directory = empty($base_directory) ? '' : JPath::clean($base_directory);
 
 		if (!JFolder::exists($this->base_directory)) {
 				JFolder::create($this->base_directory);
 				JFactory::getApplication()->enqueueMessage(JText::sprintf('COM_STATICCONTENT_HTML_CREATED_DIRECTORY_MESSAGE',$this->base_directory));
 		}
-		
+
 		if ( JFolder::exists($this->base_directory) )
 		{
-			
-			else if($this->sef == 0) {
+
+			if($this->sef == 0) {
 				$this->_layout = 'message';
 				$this->message = 'COM_STATICCONTENT_HTML_SEF_DISABLED';
 			}
@@ -35,7 +35,7 @@ class StaticContentViewStaticContent extends JView
 					$item = str_replace($path,'',$item);
 					$item = str_replace(DS,'/',$item);
 				}
-				
+
 				$folderPath = str_replace(JPATH_ROOT.DS,'',$this->base_directory);
 				$this->baseUri = JUri::root().$folderPath.'/';
 			}
@@ -44,29 +44,30 @@ class StaticContentViewStaticContent extends JView
 			$this->_layout = 'message';
 			$this->message = 'COM_STATICCONTENT_HTML_CONFIG_DIRECTORY';
 		}
-		
+
 		$this->addToolbar();
 		parent::display($tpl);
 	}
-	
+
 	protected function addToolbar()
 	{
 		$this->loadHelper('toolbar');
 		JToolBarHelper::title('Static Content');
-		
+
 		if (!empty($this->base_directory)) {
 			//if sef is enabled
 			if ($this->sef) {
 				ComStaticContentHelperToolbar::customExport();
 				ComStaticContentHelperToolbar::completeExport();
 			}
-			
+
 			if(!empty($this->items)) {
 				ComStaticContentHelperToolbar::custom('html.delete','delete','delete','COM_STATICCONTENT_TOOLBAR_DELETE', false);
 				ComStaticContentHelperToolbar::custom('html.download','archive','archive','COM_STATICCONTENT_TOOLBAR_DOWNLOAD', false);
 			}
 		}
-		
+
 		JToolBarHelper::preferences('com_staticcontent');
 	}
 }
+
